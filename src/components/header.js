7b4fa51816cd3a61763d5938f7f5134315f1bfc0
Elizabeth Doyle <myoddjourney@gmail.com>
2023-05-11 15:43:12 -0400

    4 passing tests

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,28 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+
+  const header = document.createElement('div');
+  const headerDate = document.createElement('span');
+  const headerTitle = document.createElement('h1');
+  const headerTemp = document.createElement('span');
+
+  header.classList.add('header');
+  headerDate.classList.add('date');
+  headerTemp.classList.add('temp');
+
+  headerDate.textContent = date;
+  headerTitle.textContent = title;
+  headerTemp.textContent = temp;
+
+  header.appendChild(headerDate);
+  header.appendChild(headerTitle);
+  header.appendChild(headerTemp);
+
+
+return header
+
+   
 }
 
 const headerAppender = (selector) => {
